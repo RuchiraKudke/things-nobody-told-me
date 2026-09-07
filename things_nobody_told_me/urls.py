85c8188435af0c1ada3f1b6862a239618e0f6805
RuchiraKudke <ruchirakudke@gmail.com>
2026-09-07 12:14:37 +0530

    Add password reset and authentication updates

--- things_nobody_told_me/urls.py
+++ things_nobody_told_me/urls.py
@@ -1,24 +1,82 @@
-"""
-URL configuration for things_nobody_told_me project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('knowledge.urls')),
+
+    path(
+        '',
+        views.home,
+        name='home'
+    ),
+
+    path(
+        'knowledge/<int:pk>/',
+        views.knowledge_detail,
+        name='knowledge_detail'
+    ),
+
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
+
+    path(
+        'login/',
+        views.user_login,
+        name='login'
+    ),
+
+    path(
+        'logout/',
+        views.user_logout,
+        name='logout'
+    ),
+
+    path(
+        'share-tip/',
+        views.share_tip,
+        name='share_tip'
+    ),
+
+    # =====================================================
+    # FORGOT PASSWORD
+    # =====================================================
+
+    path(
+        'forgot-password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='knowledge/password_reset.html',
+            email_template_name='knowledge/password_reset_email.html',
+            success_url='/forgot-password/done/'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'forgot-password/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='knowledge/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'reset-password/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='knowledge/password_reset_confirm.html',
+            success_url='/reset-password/complete/'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'reset-password/complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='knowledge/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
