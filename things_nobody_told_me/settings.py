49c62e288d3e3c67c41ada0a404ab5a84a8eeb96
RuchiraKudke <ruchirakudke@gmail.com>
2026-09-11 15:08:50 +0530

    Configure Resend email

--- things_nobody_told_me/settings.py
+++ things_nobody_told_me/settings.py
@@ -216,32 +216,28 @@ LOGOUT_REDIRECT_URL = "/"
 
 
 # =========================================================
-# EMAIL
+# EMAIL - RESEND
 # =========================================================
 
 EMAIL_BACKEND = (
     "django.core.mail.backends.smtp.EmailBackend"
 )
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.resend.com"
 
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get(
-    "EMAIL_HOST_USER",
-    ""
-)
+EMAIL_HOST_USER = "resend"
 
 EMAIL_HOST_PASSWORD = os.environ.get(
-    "EMAIL_HOST_PASSWORD",
+    "RESEND_API_KEY",
     ""
 )
 
-DEFAULT_FROM_EMAIL = os.environ.get(
-    "DEFAULT_FROM_EMAIL",
-    EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = (
+    "Things Nobody Told Me <onboarding@resend.dev>"
 )
 
 EMAIL_TIMEOUT = 20
